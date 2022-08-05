3d9470a423d6dabc4dce42ec8007051cfa0b65e3
Fadilah Riczky <friczky@gmail.com>
2022-08-05 20:31:35 +0700

    :zap: Membuat model plus eloquent

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Products;
 
 class Categories extends Model
 {
-    use HasFactory;
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Products::class, 'category_id');
+    }
 }
--- app/Models/Products.php
+++ app/Models/Products.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Categories;
 
 class Products extends Model
 {
-    use HasFactory;
+    protected $table = 'products';
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Categories::class, 'category_id', 'id');
+    }
 }
